19c233a380378303a4af63fafcbb7e8fa3ed93f9
Sabin Raj Dangol <sawbean.raz@googlemail.com>
2018-11-25 01:31:59 +0000

    question validation and test

--- src/core/questions.js
+++ src/core/questions.js
@@ -1,3 +1,11 @@
+/**
+ * @module mathsquiz/core
+ */
+
+
+/**
+ * @constant maths/quiz/OPERATORS
+ */
 export const OPERATORS = {
   ADDITION: '+',
   SUBSTRACTION: '-',
@@ -5,20 +13,72 @@ export const OPERATORS = {
   MULTIPLICATION: 'x',
 };
 
+/**
+ * @func mathsquiz/core/isValidOperator
+ * @desc validates if operator is validate */
+export const isValidOperator = (operator) => {
+  const operators = Object.keys(OPERATORS).map(k => OPERATORS[k]);
+  return operators.includes(operator);
+};
 
-export const add = digits => digits.reduce((a, d) => (a + d), 0);
+/**
+ * @func mathsquiz/core/isValidQuestion
+ * @desc validates if question is valid for quiz or not */
+export const isValidQuestion = (question) => {
+  const { operator, digit1, digit2 } = question;
 
+  if (operator === OPERATORS.ADDITION) {
+    return digit1 >= 0 && digit2 >= 0;
+  }
+
+  if (operator === OPERATORS.SUBSTRACTION) {
+    return digit1 > digit2;
+  }
+
+  if (operator === OPERATORS.DIVISION) {
+    return digit1 > 0 && digit2 > 0 && (digit1 % digit2 === 0);
+  }
+
+  if (operator === OPERATORS.MULTIPLICATION) {
+    return digit1 >= 0 && digit2 >= 0;
+  }
+  return false;
+};
+
+/**
+ * @func mathsquiz/core/add
+ */
+export const add = digits => digits.reduce((a, d) => {
+  if (typeof d !== 'number') throw new Error('Required to be array of numbers');
+  return (a + d);
+}, 0);
+
+/**
+ * @func mathsquiz/core/substract
+ */
 export const substract = digits => digits.slice(1)
   .reduce((a, d) => a - d, digits.slice(0, 1));
 
+/**
+ * @func mathsquiz/core/divide
+ */
 export const divide = digits => digits.slice(1)
   .reduce((a, d) => a / d, digits.slice(0, 1));
 
+/**
+ * @func mathsquiz/core/multiply
+ */
 export const multiply = digits => digits.slice(1)
   .reduce((a, d) => a * d, digits.slice(0, 1));
 
+/**
+ * @func mathsquiz/core/getRandomInt
+ */
 export const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
+/**
+ * @func mathsquiz/core/getRandomOperator
+ */
 export const getRandomOperator = () => {
   const optIndex = getRandomInt(4);
 
@@ -27,12 +87,39 @@ export const getRandomOperator = () => {
   return opt;
 };
 
-export const getQuestion = () => ({
-  digit1: getRandomInt(20),
-  digit2: getRandomInt(20),
-  operator: getRandomOperator(),
-});
+/**
+ * @func mathsquiz/core/getQuestion
+ * @desc returns question object with random number and operator
+ */
+export const getQuestion = (optr) => {
+  if (optr && !isValidOperator(optr)) {
+    throw new Error('Invalid operator');
+  }
 
+  const operator = optr || getRandomOperator();
+  const q = {
+    digit1: 0,
+    digit2: 0,
+    operator,
+  };
+
+  do {
+    q.digit1 = getRandomInt(20);
+    q.digit2 = getRandomInt(20);
+  }
+  while (!isValidQuestion(q));
+  return q;
+};
+
+/**
+ * @func mathsquiz/core/getAnswer
+ *
+ * @param operator object
+ * @param digits array[Number]
+ *
+ * @desc takes operator and digits as array and computes answer
+ * @return Number
+ */
 export const getAnswer = ({ operator, ...digits }) => {
   const arr = Object.keys(digits).map(key => digits[key]);
   /* eslint-disable no-nested-ternary */
@@ -42,4 +129,12 @@ export const getAnswer = ({ operator, ...digits }) => {
         : (operator === OPERATORS.MULTIPLICATION) ? multiply(arr) : 0);
 };
 
-export const getQuestions = numberOfQuestions => [...Array(numberOfQuestions)].map(getQuestion);
+/**
+ * @func mathsquiz/core/getQuestions
+ *
+ * @desc returns array of question objects
+ */
+export const getQuestions = (numberOfQuestions, operator) => {
+  if (operator && !isValidOperator(operator)) throw new Error('Invalid operator');
+  return [...Array(numberOfQuestions)].map(() => getQuestion(operator));
+};
--- src/core/questions.test.js
+++ src/core/questions.test.js
@@ -21,19 +21,82 @@ describe('core question', () => {
     });
   });
 
+  describe('#isValidOperator', () => {
+    it('returns false', () => {
+      ['2', '5', 0, '%', '£', '@'].forEach((operator) => {
+        expect(questions.isValidOperator(operator)).toBe(false);
+      });
+    });
+
+    it('returns true', () => {
+      Object.keys(OPERATORS).forEach((operatorKey) => {
+        expect(questions.isValidOperator(OPERATORS[operatorKey])).toBe(true);
+      });
+    });
+  });
+
+  describe('#isValidQuestion', () => {
+    it('returns false', () => {
+      console.log('TODO Valid question');
+    });
+
+    it('returns true', () => {
+      console.log('TODO Valid quesitons');
+    });
+  });
+
   describe('#get question', () => {
     it('returns question object', () => {
       const newQuestion = questions.getQuestion();
       expect(newQuestion).toBeInstanceOf(Object);
     });
-  });
 
+    it('returns invalid operator', () => {
+      // console.log(questions.getQuestion('%'));
+      expect(() => questions.getQuestion('%')).toThrow();
+    });
+
+    it('SUBSTRACTION: first digit must be greater than second', () => {
+      for (let idx = 0; idx < 100; idx += 1) {
+        const q = questions.getQuestion(OPERATORS.SUBSTRACTION);
+        expect(q.digit1).toBeGreaterThanOrEqual(q.digit2);
+      }
+    });
+
+    it('DIVISION: first digit must be division without remainder', () => {
+      for (let idx = 0; idx < 100; idx += 1) {
+        const q = questions.getQuestion(OPERATORS.DIVISION);
+        expect((q.digit1 % q.digit2)).toEqual(0);
+      }
+    });
+  });
 
   describe('#get questions', () => {
     it('returns array of questions', () => {
       const q = questions.getQuestions(100);
       expect(Array.isArray(q)).toBe(true);
       expect(q.length).toEqual(100);
+    });
+
+    it('returns invalid operator', () => {
+      expect(() => questions.getQuestions(100, '%')).toThrow();
+    });
+  });
+
+  describe('#add', () => {
+    it('adds arrays of number', () => {
+      const r = questions.add([10, 20]);
+      expect(r).toBe(30);
+      const sum1 = questions.add([1, 2, 3, 4]);
+      expect(sum1).toBe(10);
+      const sum2 = questions.add([2, 5, 20, 40]);
+      expect(sum2).toBe(67);
+    });
+
+    it('throw error on non number array', () => {
+      expect(() => questions.add(['a', 'b'])).toThrow();
+      expect(() => questions.add([0, 2, 'ab', 20])).toThrow();
+      expect(() => questions.add(10)).toThrow();
     });
   });
 });
