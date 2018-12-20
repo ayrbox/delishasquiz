81bacabcd9fe984a69c7465243e9797dc0a31a00
Sabin Raj Dangol <sawbean.raz@googlemail.com>
2018-12-20 16:40:19 +0000

    Updates

---
+++ precache-manifest.58e732ed0b83a20ca90904e36cdbca8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "910b20e5387236b3460a2192d7153a1c",
+    "url": "/static/media/bg.910b20e5.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f5fecc7c2973342bbc1",
+    "url": "/static/js/main.0f5fecc7.chunk.js"
+  },
+  {
+    "revision": "2f0cc866b127d0f16185",
+    "url": "/static/js/1.2f0cc866.chunk.js"
+  },
+  {
+    "revision": "0f5fecc7c2973342bbc1",
+    "url": "/static/css/main.f5df03d1.chunk.css"
+  },
+  {
+    "revision": "2f0cc866b127d0f16185",
+    "url": "/static/css/1.35279cec.chunk.css"
+  },
+  {
+    "revision": "059c937aa9666632cfded1b58d08ede9",
+    "url": "/index.html"
+  }
+];
